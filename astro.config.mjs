a75e9396ffd39906981cc625be3aa20cc38e1ee9
ajismaulana <ajismaulana.id@gmail.com>
2024-05-23 11:35:21 +0700

    Add island components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zys-astro-tutorial.vercel.app",
+  integrations: [preact({ devtools: true })],
 });
